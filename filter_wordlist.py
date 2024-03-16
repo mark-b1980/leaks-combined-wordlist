10f134eddbc35a85eb5f4eed9b0693e2db92fe5f
mark-b1980 <mark.b@post.cz>
2024-03-16 23:58:22 +0100

    Bugfix in filter

--- filter_wordlist.py
+++ filter_wordlist.py
@@ -18,18 +18,40 @@ with open(infile, "r", encoding="UTF-8", errors="ignore") as f:
     for line in f:
         line = line.rstrip("\n")
         # Skip emails
-        if re.match(r'[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}', line):
-            print(line)
+        if re.findall(r'[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}', line):
+            #print(line)
 
             # Check format email:password
             if ":" in line:
-                print(line)
                 tmp = line.split(":")
                 line = tmp[-1]
+            
+            # Check format email;password
+            elif ";" in line:
+                tmp = line.split(";")
+                line = tmp[-1]
+
+            # Check format email;password
+            elif "||" in line:
+                tmp = line.split("||")
+                line = tmp[-1]
+
+            # Set dummy value to prevent processing
             else:
+                print(line)
                 line = ""
 
-        if len(line) > 3 and len(line) < 24 and not line.startswith("$HEX"):
+        # Check for || as field delimiter in case of username||password
+        if "||" in line:
+            tmp = line.split("||")
+            line = tmp[-1]
+
+        # Ignore lines with $HEX[...] notation
+        if line.startswith("$HEX"):
+            print(line)
+
+        # Add all 4 - 23 character lines as password
+        elif len(line) > 3 and len(line) < 24:
             out.write(f"{line}\n")
         
         # Just numbers or all the same caracters
